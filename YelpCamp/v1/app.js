8d0fe816ae7a021097518a7a894510126cf06d0a
Ethan Liao <marineethan@gmail.com>
2018-09-22 19:29:00 +0000

    Added campgrounds page

--- YelpCamp/v1/app.js
+++ YelpCamp/v1/app.js
@@ -7,6 +7,16 @@ app.get("/", function(req, res) {
    res.render("landing");
 });
 
+app. get("/campgrounds", function(req, res) {
+    var campgrounds = [
+        {name: "Salmon Creek", image: "https://pixabay.com/get/e837b1072af4003ed1584d05fb1d4e97e07ee3d21cac104496f7c778a5ebbcb0_340.jpg"},
+        {name: "Granite Hill", image: "https://pixabay.com/get/e834b5062cf4033ed1584d05fb1d4e97e07ee3d21cac104496f7c778a5ebbcb0_340.jpg"},
+        {name: "Mountain Goat's Rest", image: "https://pixabay.com/get/e83db40e28fd033ed1584d05fb1d4e97e07ee3d21cac104496f7c778a5ebbcb0_340.jpg"}
+    ]
+    
+    res.render("campgrounds");
+});
+
 app.listen(process.env.PORT, process.env.IP, function() {
     console.log("The YelpCamp Server Has Started!");
 });
